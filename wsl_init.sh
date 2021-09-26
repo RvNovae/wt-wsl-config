cb45e1cd3430ef5b3648939c67c1c42d8ae590f3
Oliver Kogel <kogel.oliver@web.de>
2021-09-26 21:26:33 +0200

    Update wsl_init.sh

--- wsl_init.sh
+++ wsl_init.sh
@@ -1,3 +1,8 @@
+# make windows directories readable when using ls
 echo 'LS_COLORS="ow=01;36;40" && export LS_COLORS' >> ~/.bashrc
+
+# use explorer without .exe to open a directory in explorer
 echo 'alias explorer="explorer.exe"' >> ~/.bashrc
+
+# reload shell
 source ~/.bashrc
